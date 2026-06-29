d6dafde17e1b8698d848e737f15c628e0e2270d8
Aditya <aditya.d0518@gmail.com>
2026-06-29 18:01:19 +0530

    implemented Cloudinary image upload API

--- cms/src/main/java/com/insighthub/cms/controller/ImageController.java
+++ cms/src/main/java/com/insighthub/cms/controller/ImageController.java
@@ -1,4 +1,20 @@
 package com.insighthub.cms.controller;
-
+import com.insighthub.cms.dto.ImageUploadResponse;
+import com.insighthub.cms.service.CloudinaryService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+@RestController
+@RequestMapping("/images")
 public class ImageController {
+    private final CloudinaryService cloudinaryService;
+    public ImageController(CloudinaryService cloudinaryService){
+        this.cloudinaryService = cloudinaryService;
+    }
+    @PostMapping("/upload")
+    @PreAuthorize("hasRole('AUTHOR')")
+    public ImageUploadResponse uploadImage(@RequestParam("file") MultipartFile file){
+        String imageUrl = cloudinaryService.uploadImage(file);
+        return new ImageUploadResponse(imageUrl);
+    }
 }
